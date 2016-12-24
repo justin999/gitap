3d1d98a4460d6e56d6519f0ae268eaabe69f95b5
justin999 <koichi.sato.aow@gmail.com>
2016-12-24 12:05:16 +0900

    make tabcontrollers work

--- gitap/FeedsViewController.swift
+++ gitap/FeedsViewController.swift
@@ -8,11 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FeedsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor.blue
     }
 
     override func didReceiveMemoryWarning() {
---
+++ gitap/ReposViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ReposViewController.swift
+//  gitap
+//
+//  Created by Koichi Sato on 12/24/16.
+//  Copyright © 2016 Koichi Sato. All rights reserved.
+//
+
+import UIKit
+
+class ReposViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.green
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
