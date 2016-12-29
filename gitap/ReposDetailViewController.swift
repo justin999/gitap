6f7d7d30dc940d0eba9681e4cd2259057db86b16
justin999 <koichi.sato.aow@gmail.com>
2016-12-29 19:39:48 +0900

    add segmentedControl to ReposDetailVC

--- gitap/ReposDetailViewController.swift
+++ gitap/ReposDetailViewController.swift
@@ -10,16 +10,44 @@ import UIKit
 
 class ReposDetailViewController: UIViewController {
     var stateController: StateController?
+//    var segmentedControl: UISegmentedControl?
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        configureView()
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.tabBarController?.tabBar.isHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        self.tabBarController?.tabBar.isHidden = false
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    
+    func configureView() {
+        segmentedControl.selectedSegmentIndex = 0
+        segmentedControl.addTarget(self, action: #selector(ReposDetailViewController.segmentValueChanged(segCon:)), for: .valueChanged)
+        navigationController?.title = "Repo Name"
+    }
+    
+    func segmentValueChanged(segCon:UISegmentedControl) {
+        switch segCon.selectedSegmentIndex {
+        case 0:
+            print("Issues selected")
+        case 1:
+            print("Pull Requests selected")
+        default:
+            print("came to default")
+        }
+    }
     
     func addButtonTapped() {
         print("add button tapped")
