b629b5c7c41548613681dad2dab542af5176f990
justin999 <koichi.sato.aow@gmail.com>
2016-12-30 11:48:12 +0900

    update settings view controller

--- gitap/SettingsTableViewDataSource.swift
+++ gitap/SettingsTableViewDataSource.swift
@@ -11,6 +11,17 @@ import UIKit
 class SettingsTableViewDataSource: NSObject {
     var stateController: StateController
     
+    let cellInfo: [Dictionary<String, Array<String>>] =
+        [
+    ["sectionName": ["account"],
+     "items": ["push notification","github auth"]
+            ],
+    
+    ["sectionName": ["gitap"],
+     "items": ["feedback", "follow on twitter", "upgrades", "donate", "app version"]
+            ]
+    ]
+    
     init(tableView: UITableView, stateController: StateController) {
         self.stateController = stateController
         super.init()
@@ -20,19 +31,27 @@ class SettingsTableViewDataSource: NSObject {
 }
 
 extension SettingsTableViewDataSource: UITableViewDataSource {
+    // setting sections
+    // acount, gitap
+    // account section -> push notifictaion, github auth
+    // gitap -> feedback, follow on twitter, upgrades, donate, app version
+    // feedbackシステムは工夫したい。星を選ばせて、3star以下だったらアプリ制作者にメールを飛ばすように。4以上だったらApp Storeにも流すようにしたい。
+    
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 2
+        return cellInfo.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        let cell = UITableViewCell()
+        cell.textLabel?.text = cellInfo[indexPath.section]["items"]?[indexPath.row]
+        return cell
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return (cellInfo[section]["items"]?.count)!
     }
     
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return "account" //account, support
+        return cellInfo[section]["sectionName"]?[0]
     }
 }
