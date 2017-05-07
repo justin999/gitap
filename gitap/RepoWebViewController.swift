7a88239e575c976f21b101057018085844956ce3
justin999 <koichi.sato.aow@gmail.com>
2017-05-07 23:17:30 +0900

    remove lines

--- gitap/RepoWebViewController.swift
+++ gitap/RepoWebViewController.swift
@@ -13,12 +13,10 @@ class RepoWebViewController: SFSafariViewController, SFSafariViewControllerDeleg
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 }
