a4629256dc8096c6e67d504b111cc576a8dd9827
justin999 <koichi.sato.aow@gmail.com>
2017-11-20 19:55:46 +0900

    update segue

--- gitap/ReposViewController.swift
+++ gitap/ReposViewController.swift
@@ -52,14 +52,15 @@ class ReposViewController: UIViewController {
     }
     
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "presentEditIssue",
+            let destiantion = segue.destination as? UINavigationController,
+            let vc = destiantion.topViewController as? CreateIssuesViewController {
+            vc.stateController = stateController
+        }
     }
-    */
-
 }
