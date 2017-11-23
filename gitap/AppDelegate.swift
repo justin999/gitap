cfeb7f5c36979107b180be99d2199935bfe5aa80
justin999 <koichi.sato.aow@gmail.com>
2017-11-23 21:12:14 +0900

    tweak

--- gitap/AppDelegate.swift
+++ gitap/AppDelegate.swift
@@ -27,6 +27,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
         return true
     }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+        GitHubAPIManager.shared.processOAuthStep1Response(url)
+        return true
+    }
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
@@ -48,13 +53,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
-
-    
-    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
-        GitHubAPIManager.shared.processOAuthStep1Response(url)
-        return true
     }
     
 }
