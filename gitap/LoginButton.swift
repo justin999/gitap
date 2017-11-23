d997f8b8296709e0bf912aab995c6bddc1ac196d
justin999 <koichi.sato.aow@gmail.com>
2017-11-23 16:59:04 +0900

    add corner radius to login button

--- gitap/LoginButton.swift
+++ gitap/LoginButton.swift
@@ -7,3 +7,11 @@
 //
 
 import Foundation
+import UIKit
+
+class LoginButton: UIButton {
+    override func layoutSubviews() {
+        self.layer.cornerRadius = 10
+        super.layoutSubviews()
+    }
+}
